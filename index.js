dcda35c8c5080b63f4195a631488d1567203faf8
hoto17296 <mail@hoto.me>
2017-03-17 14:46:30 +0900

    impl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const SlackInviteForm = require('./lib/SlackInviteForm');
 
-window.renderSlackInviteForm = function(elem) {
+window.renderSlackInviteForm = function(elem, api_base) {
   if (!(elem instanceof HTMLElement)) elem = document.getElementById(elem);
-  ReactDOM.render(React.createElement(SlackInviteForm), elem);
+  ReactDOM.render(React.createElement(SlackInviteForm, { api_base }), elem);
 };
